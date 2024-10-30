dd3819d3f16c6c954672781ef8320d0faf64661d
DBG <dbg@dbg.es>
2024-10-30 18:20:45 +0000

    biblioteca commit

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "http://10.2.1.118/biblioteca/";
+const host = "localhost";
+const user = "biblioteca";
+const pass = "usuario";
+const db = "biblioteca";
+const charset = "charset=utf8";
+?>
